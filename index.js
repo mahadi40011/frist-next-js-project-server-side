e21129611e6299941d6adf2c9d0ecca2bd027394
mahadi40011 <mahadi40011@gmail.com>
2025-11-27 10:50:19 +0600

    Implement GET method

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
       res.send(result);
     });
 
+    // get all data
+    app.get("/products", async (req, res) => {
+      let cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
