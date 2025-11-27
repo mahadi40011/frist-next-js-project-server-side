e045981dfa56573811f2345079aa72bf8f3dc7c4
mahadi40011 <mahadi40011@gmail.com>
2025-11-27 11:04:51 +0600

    Implement DELETE method for delete 1 data

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a data
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ProductCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
